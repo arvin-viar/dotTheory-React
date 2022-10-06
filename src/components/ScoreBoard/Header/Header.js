6cb4c2cc63d6da70c0663f2d1aae06ab05667cc0
Arvin Viar <arvin.viar@codeandtheory.com>
2022-10-06 21:32:48 +0800

    Apply change request

--- src/components/ScoreBoard/Header/Header.js
+++ src/components/ScoreBoard/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Header = ({ title, totalPlayers }) => <header>
-  <h1>{ title }</h1>
-  <span className="stats">
-    Players: {totalPlayers}
-  </span>
-</header>;
+const Header = ({ title, totalPlayers }) => (
+  <header>
+    <h1>{ title }</h1>
+    <span className="stats">
+      Players: {totalPlayers}
+    </span>
+  </header>
+);
 
 Header.propTypes = {
   title: PropTypes.string,
