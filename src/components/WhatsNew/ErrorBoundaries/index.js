aa377a3b4fc31d9dbf4d9790c485f3e96a9cab59
Arvin Viar <arvin.viar@codeandtheory.com>
2023-02-01 17:40:36 +0800

    Apply code change request

--- src/components/WhatsNew/ErrorBoundaries/index.js
+++ src/components/WhatsNew/ErrorBoundaries/index.js
@@ -3,20 +3,18 @@ import React from 'react';
 import StudentForm from "../StudentForm";
 import ErrorBoundary from '../../ErrorBoundary';
 
-const ErrorBoundaries = () => {
-  return (
-    <div className="content-wrapper">
-      <div className="content-header">
-        <h1 className="content-header__title">
-          Error Boundaries
-        </h1>
-      </div>
-
-      <ErrorBoundary>
-        <StudentForm />
-      </ErrorBoundary>
+const ErrorBoundaries = () => (
+  <div className="content-wrapper">
+    <div className="content-header">
+      <h1 className="content-header__title">
+        Error Boundaries
+      </h1>
     </div>
-  );
-}
+
+    <ErrorBoundary>
+      <StudentForm />
+    </ErrorBoundary>
+  </div>
+);
 
 export default ErrorBoundaries;
